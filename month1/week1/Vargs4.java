3731c2aad1259ae83c985687a2c585b579070ee0
uttej-07 <70064240+uttej-07@users.noreply.github.com>
2023-02-09 13:27:34 +0530

    Add files via upload

---
+++ month1/week1/Vargs4.java
@@ -0,0 +1,13 @@
+class Ex3{
+   public void show(int ... a,String ... name){
+    for(int i:a){
+      System.out.println(a+"name");
+          }
+         }
+        }
+class Vargs4{
+   public static void main(String[] args){
+   Ex3 m = new Ex3();
+   m.show(1,2,3,"uttej");
+   }
+}
