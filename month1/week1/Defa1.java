ecebfc30fd756e75596789c6c662ff984259a3cf
uttej-07 <70064240+uttej-07@users.noreply.github.com>
2023-02-09 19:07:54 +0530

    Add files via upload

---
+++ month1/week1/Defa1.java
@@ -0,0 +1,11 @@
+class Ex2{
+     void show(){
+          System.out.println("Hello World!");
+                }
+              }
+   class Defa1{
+      public static void main(String[] args){
+            Ex2 m = new Ex2();
+              m.show();
+                }
+             }
---
+++ month1/week1/Pub1.java
@@ -0,0 +1,13 @@
+class p1{
+   public void show(){
+    System.out.println("Hello world!");
+      }
+  }
+class Pub1{
+  public static void main(String[] args){
+    p1 m = new p1();
+    m.show();
+     }
+  }
+
+ 
