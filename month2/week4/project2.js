f3084d2ebb78424ee6b657384d7352c5333fe717
uttej-07 <70064240+uttej-07@users.noreply.github.com>
2023-03-21 16:04:27 +0530

    DOM Program Files
    Uploaded DOM program files

---
+++ month2/week4/project2.js
@@ -0,0 +1,26 @@
+//Variables
+
+let openBtn =  document.getElementById('open-btn');
+let modalContainer = document.getElementById('modal-container');
+let closeBtn = document.getElementById('close-btn');
+
+//Event Listeners
+
+openBtn.addEventListener('click', function(){
+
+    modalContainer.style.display = 'block';
+
+});
+
+closeBtn.addEventListener('click', function(){
+
+    modalContainer.style.display = 'none'; 
+
+});
+
+window.addEventListener('click', function(){
+
+    if(e.target === modalContainer){
+        modalContainer.style.display = 'none';
+    }
+});
---
+++ month2/week4/project5.js
@@ -0,0 +1,46 @@
+//Variables
+
+const addTask = document.getElementById('add-task');
+const taskContainer = document.getElementById('task-container');
+const inputTask = document.getElementById('input-task');
+
+//Event Listener for Add Button
+
+addTask.addEventListener('click', function(){
+
+    let task = document.createElement('div');
+    task.classList.add('task');
+
+    let li = document.createElement('li');
+    li.innerText = `${inputTask.value}`;
+    task.appendChild(li);
+
+    let checkBtn = document.createElement("button");
+    checkBtn.innerHTML =  '<i class = " fa-solid fa-check"></i>';
+    checkBtn.classList.add('checkTask');
+    task.appendChild(checkBtn);
+    
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML =  '<i class = " fa-solid fa-trash-can"></i>';
+    deleteBtn.classList.add('deleteTask');
+    task.appendChild(deleteBtn);
+
+    if(inputTask.value === ""){
+        alert("Please Enter a Task");
+    }else{
+        taskContainer.appendChild(task);
+    }
+    
+
+    inputTask.value = "";
+
+    checkBtn.addEventListener('click', function(){
+
+        checkBtn.parentElement.style.textDecoration = "line-through";
+    });
+
+    deleteBtn.addEventListener('click',function(e){
+          let target = e.target;
+          target.parentElement.parentElement.remove();
+    });
+});
