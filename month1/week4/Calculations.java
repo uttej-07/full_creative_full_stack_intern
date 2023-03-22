a824df70026c5be67fd8721f9c848aa9bd0e8c5e
uttej-07 <70064240+uttej-07@users.noreply.github.com>
2023-03-22 12:58:39 +0530

    Uploaded files related to Packages

---
+++ month1/week4/Calculations.java
@@ -0,0 +1,15 @@
+import packagepractice.Calculator;
+
+public class Calculations {
+
+	public static void main(String[] args) {
+
+		Calculator a = new Calculator();
+		System.out.println(a.add(5, 5));
+		System.out.println(a.sub(10, 5));
+		System.out.println(a.mul(5, 5));
+		System.out.println(a.div(10, 2));
+
+	}
+
+}
--- month1/week4/Calculator.java
+++ month1/week4/Calculator.java
@@ -1,12 +1,20 @@
-package JARPACKAGE;
+package packagepractice;
 
 public class Calculator {
 
-	public int add(int a,int b) {
+	public int add(int a, int b) {
 		return a+b;
 	}
-   
-	public int sub(int a,int b) {
+	
+	public int sub(int a, int b) {
 		return a-b;
+	}
+	
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	
+	public int div(int a, int b) {
+		return a/b;
 	}
 }
